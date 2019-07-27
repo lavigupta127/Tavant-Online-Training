e1cd377a80a0e4d5b5f935c003041135427b8ef9
lavigupta127 <53165849+lavigupta127@users.noreply.github.com>
2019-07-27 12:36:37 +0530

    Create GetHelloWorld.java

---
+++ GetHelloWorld.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class GetHelloWorld {
+	String name;
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public static void main(Sftring arwgfs[])
+	{
+		GetHelloWorld p=new GetHelloWorld();
+		p.setName("HelloWorld");
+		p.getName();
+	}
+}
+
+
+
+
+
+
+
+
